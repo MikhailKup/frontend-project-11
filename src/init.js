a4e8c4499a79f3adb9145c9ea413dd9583784ef1
MikhailKup <mifffa.mk@gmail.com>
2024-03-04 07:11:12 +0500

    Step 6. Complete init

--- src/init.js
+++ src/init.js
@@ -1,8 +1,12 @@
 import i18next from 'i18next';
 import axios from 'axios';
 import _ from 'lodash';
+import initView from './view.js';
+import ru from './locales/ru.js';
 import validateURL from './helpers/validator.js';
+import fetchData from './helpers/fetchData.js';
 import getParsedXML from './helpers/parser.js';
+import updatePosts from './helpers/updater.js';
 
 
 export default () => {
@@ -42,7 +46,53 @@ export default () => {
 		},
 	});
 
-	const watchedState = {};
+	const watchedState = initView(initialState, elements, i18n);
 
+	elements.form.addEventListener('submit', (e) => {
+		e.preventDefault();
+		watchedState.rssForm.state = 'filling';
+		const formData = new FormData(e.target);
+		const url = formData.get('url');
+		const urlsList = watchedState.feeds.map((feed) => feed.url);
+		validateURL(url, urlsList, i18n)
+		  	.then((validUrl) => {
+				watchedState.rssForm.error = null;
+				watchedState.rssForm.state = 'processing';
+				return fetchData(validUrl);
+		  	})
+		  	.then(({ data }) => {
+				const [feed, posts] = getParsedXML(data.contents);
+				const newFeed = { ...feed, id: _.uniqueId(), url };
+				const newPosts = posts.map((post) => ({ ...post, id: _.uniqueId(), feedId: newFeed.id }));
+				watchedState.feeds = [newFeed, ...watchedState.feeds];
+				watchedState.posts = [...newPosts, ...watchedState.posts];
+				watchedState.rssForm.state = 'success';
+		  	})
+			.catch((err) => {
+				watchedState.rssForm.valid = err.name !== 'ValidationError';
+				if (err.name === 'ValidationError') {
+					watchedState.rssForm.error = err.message;
+				} else if (err.NotValidRss) {
+					watchedState.rssForm.error = 'form.errors.notValidRss';
+				} else if (axios.isAxiosError(err)) {
+					watchedState.rssForm.error = 'form.errors.networkProblems';
+				}
+				watchedState.rssForm.state = 'filling';
+			});
+	});
+
+	elements.postsContainer.addEventListener('click', ({ target }) => {
+		if (target.closest('a')) {
+			const { id } = target.dataset;
+			watchedState.uiState.visitedPosts.add(id);
+		}
+		if (target.closest('button')) {
+			const { id } = target.dataset;
+			watchedState.uiState.visitedPosts.add(id);
+			watchedState.uiState.modalId = id;
+		}
+	});
+
+	setTimeout(() => updatePosts(watchedState), 5000);
 };
 
